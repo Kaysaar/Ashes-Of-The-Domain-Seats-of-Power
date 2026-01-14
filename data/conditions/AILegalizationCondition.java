de0411e32bb114c23fdd4e8143203e889d9e5798
NebulaModLab <NebulaModding@hotmail.com>
2026-01-15 00:07:51 +0100

    AI Legalization access > income modifier fix

--- data/conditions/AILegalizationCondition.java
+++ data/conditions/AILegalizationCondition.java
@@ -7,16 +7,16 @@ public class AILegalizationCondition extends PolicyApplierCondition{
     public void apply(String id) {
        if(market.getAdmin()!=null&&market.getAdmin().isAICore()){
            market.getStability().modifyFlat("ai_core_legal",2,"AI Legalization");
-           market.getAccessibilityMod().modifyFlat("ai_core_legal",0.2f,"AI Legalization");
+           market.getIncomeMult().modifyMult("ai_core_legal",1.3f,"AI Legalization");
 
        }
-       market.getIndustries().stream().filter(x->x.getAICoreId()!=null).forEach(x->x.getSupplyBonusFromOther().modifyFlat("ai_core_legal",1,"Ai Core Legalization"));
+       market.getIndustries().stream().filter(x->x.getAICoreId()!=null).forEach(x->x.getSupplyBonusFromOther().modifyFlat("ai_core_legal",1,"AI Legalization"));
     }
 
     @Override
     public void unapply(String id) {
         market.getStability().unmodifyFlat("ai_core_legal");
-        market.getAccessibilityMod().unmodifyFlat("ai_core_legal");
+        market.getIncomeMult().unmodifyMult("ai_core_legal");
         market.getIndustries().forEach(x->x.getSupplyBonusFromOther().unmodifyFlat("ai_core_legal"));
 
     }
